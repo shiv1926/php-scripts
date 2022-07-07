5640891f45fa48eeaada6e982ab6d251357bebc7
shivkumar yadav <shiv.yadav26@gmail.com>
2022-07-07 09:03:19 +0530

    index

--- index.php
+++ index.php
@@ -17,5 +17,15 @@
 			<li><a href="<?php echo ex_url('scope-variable.php'); ?>">Scope of Variables</a></li>
 		</ul>
 	</li>
+	<li>
+		<h4>PHP Section Refrences : check this from w3schools</h4>
+		<ul>
+			<li><a href="<?php echo ex_url('variable-introduction.php'); ?>">All array functions</a></li>
+			<li><a href="<?php echo ex_url('variable-declaration.php'); ?>">All string functions</a></li>
+			<li><a href="<?php echo ex_url('variable-comparison.php'); ?>">All file handling functions</a></li>
+			<li><a href="<?php echo ex_url('scope-variable.php'); ?>">All iteration function, loop, map, filter, reduce</a></li>
+			<li><a href="<?php echo ex_url('scope-variable.php'); ?>">All timezone and their functions</a></li>
+		</ul>
+	</li>
 </ul>
 <?php include('footer.php'); ?>
